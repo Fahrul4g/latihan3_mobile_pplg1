4496df71f0d8cdf31ef405fac770ce3ab2838d8f
Fahrul4g <fahrulirawan554@gmail.com>
2024-09-02 12:16:28 +0700

    commit pertama

---
+++ lib/page2.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class page2 extends StatelessWidget {
+  const page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        Row(
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.red,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.green,
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.blue,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.amber,
+            ),
+          ],
+        )
+      ],
+    ));
+  }
+}
